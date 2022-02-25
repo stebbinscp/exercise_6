1e64bc4c4df94ef233e0e3b35bbfe4589f6c1085
stebbinscp <cstebbins@uchicago.edu>
2022-02-24 23:34:01 -0600

    Code written now time to debug

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import string
 import random
 from datetime import datetime
@@ -7,27 +8,133 @@ from functools import wraps
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-# You can store data on your server in whatever structure is most convenient,
-# either holding it in memory on your server or in a sqlite database.
-# You may use the sample structures below or create your own.
+### Global data ###
 
-def newChat(host_auth_key):
-    magic_passphrase = ''.join(random.choices(string.ascii_lowercase + string.digits, k=40))
+## User dict of username and password combination
+USERS = {}
 
-    return dict([
-        ("authorized_users", set(host_auth_key)),
-        ("magic_key", magic_passphrase),
-        ("messages", [])
-    ])
+# chat dict with keys magic_passphrase, authorized (usernames), 
+# messages => list of dict username, message
+CHATS = {}
 
+# dict of the form magic_passphrase: real chat id
+MAGIC_CHATS = {}
 
-# TODO: Include any other routes your app might send users to
+# dict of the form authkey: username to be able to reconstruct the user who visits
+AUTHKEY_USER = {}
+
 @app.route('/')
-@app.route('/chat/<int:chat_id>')
-def index(chat_id=None):
-    return app.send_static_file('index.html')
+def index_html():
+    return render_template('/index.html')   
 
+@app.route('/signup')
+def signup():
+    username = request.args['username']
+    password = request.args['password']
+    # get username and passcode
+    if username in USERS.keys():
+        return {"message": "user already exists"}
+        # enforce uniqueness
+    else:
+        USERS[username] = password
+        # save the username and passcode combo
+        authkey = ''.join(random.choices(string.ascii_lowercase + string.digits, k=15))
+        AUTHKEY_USER['authkey'] = username
+        # save the authkey to the username 
+        # return authkey for future requests
+        return {"authkey": authkey}
 
-# -------------------------------- API ROUTES ----------------------------------
+@app.route('/login')
+def login():
+    username = request.args['username']
+    password = request.args['password']
+    if username not in USERS.keys():
+        return {"message": "user does not exist"}
+    else:
+        if USERS[username] != password:
+            return {"message": "password incorrect"}
+        else:
+            # if user and password match
+            authkey = ''.join(random.choices(string.ascii_lowercase + string.digits, k=15))
+            AUTHKEY_USER['authkey'] = username
+            # save authkey to username and return
+            return {"authkey": authkey}
+        
+@app.route('/chat_list')
+def get_chat_list():
+    authkey = request.args['authkey']
+    return_chats = []
+    if len(CHATS) > 0:
+        for chat in CHATS:
+            # if the username associated with the authkey is in the CHATS chat authorized
+            if AUTHKEY_USER[authkey] in CHATS[chat]["authorized"]:
+                # add to the chat list for the user
+                return_chats.append(CHATS[chat]["chat_id"])
+                # return all chats for that user
+    return {"chats": return_chats}
 
-# TODO: Create the API
+@app.route('/create_chat')
+def create_chat():
+    if authkey in request.args:
+        # ensure we have a key and an authorized user
+        authkey = request.args['authkey']
+        username = AUTHKEY_USER[authkey]
+    else:
+        return {"message": "error"}
+    chat_id = len(CHATS) + 1
+    # generate new chat id and passphrase
+    magic_passphrase = ''.join(random.choices(string.ascii_lowercase + string.digits, k=15))
+    CHATS[chat_id] = {}
+    # init authorized users
+    CHATS[chat_id]["authorized"] = [username]
+    CHATS[chat_id]["messages"] = []
+    # store magic passphrase
+    CHATS[chat_id]['magic'] = magic_passphrase
+    MAGIC_CHATS[magic_passphrase] = chat_id
+    return {"chat_id": chat_id, "magic_passphrase": magic_passphrase}
+
+@app.route('/chat/<chat_id>', methods=['GET'])
+def get_chat(chat_id):
+    if authkey in request.args:
+        authkey = request.args['authkey']
+        username = AUTHKEY_USER[authkey]
+    if chat_id in CHATS.keys():
+        # real chat 
+        # get the magic passcode so we can return it to render
+        if username in CHATS[chat_id]['authorized']:
+            return {
+                "messages": CHATS[chat_id]['messages'],
+                "chat_id": chat_id,
+                'magic_passphrase':CHATS[chat_id]['magic_passphrase']
+            }
+    elif chat_id in MAGIC_CHATS.keys():
+        # magic passcode
+        # get the original chat with MAGIC_CHATS[chat_id] and authorize user
+        # pass the chat id back and the magic pass
+        CHATS[MAGIC_CHATS[chat_id]]["authorized"].append(username)
+        return {
+            "messages": CHATS[MAGIC_CHATS[chat_id]],
+            "chat_id": MAGIC_CHATS[chat_id],
+            'magic_passphrase': chat_id
+            }
+    else: return {'message': 'chat id did not match in real ids or in magic passphrases'}
+
+@app.route('/chat/<chat_id>', methods=['POST'])
+def post_chat(chat_id):
+    if authkey in request.args:
+        authkey = request.args['authkey']
+        username = AUTHKEY_USER[authkey]
+        message = request.args['message']
+    else:
+        return {"message": "error"}
+    for chat in CHATS:
+        if username in CHATS[chat]["authorized"]:
+            CHATS[chat_id]["messages"].append(
+                {
+                    "username": username,
+                    "message": message
+                }
+            )
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
--- static/script.js
+++ static/script.js
@@ -2,8 +2,146 @@
 
 // TODO: If a user clicks to create a chat, create an auth key for them
 // and save it. Redirect the user to /chat/<chat_id>
-function createChat() {
 
+// function checkAuth() {
+//   const myStorage = window.localStorage;
+//   if ("authkey" in Object.keys(myStorage)) {
+    // hit backend to check if authkey is valid
+    // if not valid
+      // clear storage authkey
+      // direct to login
+    // if it is
+      // hit the backend for the chats registered to the user
+      // render a list of them
+  // } else {
+    // request username and password
+    // return authkey
+    // render index of chats
+  // }
+  // fetch(currentUrl+'/check_creds?'+ new URLSearchParams({
+  //   username: myStorage.getItem('username'),
+  //   authkey: myStorage.getItem('authkey')
+  // }))
+  // .then(response => response.json())
+  // .then(data => {
+  //   console.log(data);
+  //   if (data.message !== "works") {
+  //     window.location.replace("/");
+  //   }
+  // })
+// }
+
+function login() {
+  console.log("clicked");
+  console.log(document.getElementById("username").value);
+  console.log(document.getElementById('password').value);
+  // get username and passcode
+
+  fetch('/login?'+ new URLSearchParams({
+      username: document.getElementById('username').value,
+      password: document.getElementById('password').value
+    }))
+    // send username and passcode to backend
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      if (!"authkey" in Object.keys(data)) {
+        console.log("failed");
+        // if we do not get an authkey, we have failed
+      } else {
+        myStorage = window.localStorage;
+        myStorage.setItem("authkey", authkey);
+        // save authkey
+        console.log("successful");
+        document.getElementById("auth").style.display = "none";
+        // remove auth from view
+        document.getElementById("chat_index").style.display = "block";
+        // displace the chat index
+
+        fetch('/chat_list?'+ new URLSearchParams({
+          authkey: document.getElementById('authkey').value
+          // send authkey to get the username's chats
+        }))
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          console.log("successful");
+          const chats = datum.chats;
+          const chat_index = document.getElementById("chat_index");
+          // map each chat
+          chats.map((chat) => {
+            var text = document.createTextNode("chat id: "+chat);
+            text.addEventListener("onClick", getMessages());
+            // onclick event added to the text node which cal
+            chat_index.appendChild(text);
+          })
+        })
+      }
+    })
+}
+
+function signup() {
+  console.log(document.getElementById("username").value);
+  console.log(document.getElementById('password').value);
+
+  fetch('/signup?'+ new URLSearchParams({
+      username: document.getElementById('username').value,
+      password: document.getElementById('password').value
+    }))
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      if (!"authkey" in Object.keys(data)) {
+        console.log("failed");
+      } else {
+        myStorage = window.localStorage;
+        myStorage.setItem("authkey", data.authkey);
+        console.log("successful");
+        document.getElementById("auth").style.display = "none";
+        document.getElementById("chat_index").style.display = "block";
+
+        fetch('/chat_list?'+ new URLSearchParams({
+          authkey: document.getElementById('authkey').value
+        }))
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          console.log("successful");
+          const chats = datum.chats;
+          const chat_index = document.getElementById("chat_index");
+          chats.map((chat) => {
+            var text = document.createTextNode("chat id: "+chat);
+            text.addEventListener("onClick", getMessages())
+            chat_index.appendChild(text);
+          })
+          document.getElementById("auth").style.display = "none";
+          document.getElementById("chat_index").style.display = "block";
+        })
+      }
+    })
+}
+
+function createChat() {
+  console.log("clicked create chat");
+  myStorage = window.localStorage;
+  authkey = myStorage.getItem('authkey');
+  fetch('/create_chat?'+ new URLSearchParams({
+    authkey: authkey
+  }))
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    console.log("successful");
+    const chat_id = data.chat_id;
+    document.getElementById("auth").style.display = "none";
+    document.getElementById("chat_index").style.display = "none";
+    // auto put the user at the chat page
+    console.log(data.magic_passphrase);
+    document.getElementById('invite_link').innerHTML = "chat/"+magic_passphrase;
+      // magic passphrase to the link area
+    window.history.pushState('', 'Chat '+chat_id, '/chat/'+chat_id);
+    getMessages();
+  })
 }
 
 /* For chat.html */
@@ -12,13 +150,45 @@ function createChat() {
 // POST to the API when the user posts a new message.
 // Automatically poll for new messages on a regular interval.
 function postMessage() {
+  myStorage = window.localStorage;
+  authkey = myStorage.getItem('authkey');
+  const chat_id = window.location.href.split("/")[3];
+  fetch('/chat/'+chat_id+"?"+ new URLSearchParams({
+    authkey: authkey
+  }), {method: 'POST'})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    console.log("successful");
+  })
   return;
 }
 
 function getMessages() {
+  myStorage = window.localStorage;
+  if ('authkey' in Object.keys(myStorage)) {
+    authkey = myStorage.getItem('authkey');
+    const chat_id = window.location.href.split("/")[3];
+    fetch('/chat/'+chat_id+"?"+ new URLSearchParams({
+      authkey: authkey
+    }))
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      console.log("successful");
+      document.getElementById("auth").style.display = "none";
+      document.getElementById("chat_index").style.display = "none";
+      document.getElementById("chat_section").style.display = "block";
+      window.history.pushState('', 'Chat '+chat_id, '/chat/'+chat_id);
+    })
+  } else {
+    console.log('interval');
+  }
   return;
 }
 
 function startMessagePolling() {
+  // setInterval(getMessages(), 1000)
+  setInterval(console.log('interval'), 5000);
   return;
 }
